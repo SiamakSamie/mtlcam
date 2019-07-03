8c5e2c822f0c51b65ec39593110c84dbc17c96e3
Siamak Samie <siamak.samie@gmail.com>
2019-07-03 14:19:07 -0400

    skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+//components
+import Header from "./components/header";
+import Catalog from "./components/catalog";
+import Footer from "./components/footer";
+
+//css
+import "./assets/css/main.css";
+import "./assets/css/header.css";
+import "./assets/css/footer.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Catalog />
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Count Up To The Moon</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+      <Footer />
     </div>
   );
 }
